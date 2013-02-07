747b78da44196748e726ad25a26ec5961533fe82
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2013-02-07 13:53:41 -0500

    increment version number

--- crashmail/version.h
+++ crashmail/version.h
@@ -2,9 +2,9 @@
 #define _VERSION_H
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 83
-#define VERSION "0.83"
-#define TID_VERSION "0.83"
+#define VERSION_MINOR 84
+#define VERSION "0.84"
+#define TID_VERSION "0.84"
 
 #endif /* _VERSION_H */
 
