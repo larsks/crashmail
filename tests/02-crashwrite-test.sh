29dde8ba93f948fa0db4f0893dc5cd4c72ad5667
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2013-02-13 12:54:37 -0500

    added tests for named packets

--- tests/02-crashwrite-test.sh
+++ tests/02-crashwrite-test.sh
@@ -6,10 +6,12 @@ describe "crashwrite"
 
 before () {
 	setup_crashmail_env
+	setup_tmpfile
 }
 
 after () {
 	clean_crashmail_env
+	clean_tmpfile
 }
 
 it_generates_a_packet () {
@@ -18,6 +20,21 @@ it_generates_a_packet () {
 		FROMADDR "99:99/1" \
 		TONAME "Test User" \
 		TOADDR "99:99/99" \
-		SUBJECT "Test Message"
+		SUBJECT "Test Message" | tee $tmpfile
+	
+	pkt=$(awk '/^Writing:/ {print $2}' $tmpfile)
+	test -f $pkt
+}
+
+it_generates_named_packet () {
+	$__tools__/crashwrite DIR spool/temp \
+		FILENAME test.pkt \
+		FROMNAME "Test Sysop" \
+		FROMADDR "99:99/1" \
+		TONAME "Test User" \
+		TOADDR "99:99/99" \
+		SUBJECT "Test Message" | tee $tmpfile
+	
+	test -f spool/temp/test.pkt
 }
 
